9edd0960c79c0e9ff055f5655626647ff65e8201
blackmantram <misantropee@gmail.com>
2015-10-22 12:09:14 -0500

    added more test cases

--- spec/updaterSpec.js
+++ spec/updaterSpec.js
@@ -3,7 +3,16 @@ var Item = require('../lib/item.js');
 
 describe("updater-test", function() {
 
-	updater = new Updater();
+	var updater;
+
+	function updateItem(item) {
+		updater.addItem(item);
+		updater.update(updater.items);
+	}
+
+	beforeEach(function(){
+		updater = new Updater();
+	});
 
 	it("can add items", function() {
 		updater.addItem({});
@@ -14,11 +23,32 @@ describe("updater-test", function() {
 		expect(updater.update).toBeDefined();
 	});
 
-	it("item quality decreases on update", function() {
-		item = new Item('item');
+	it("decreases item quality on update", function() {
+		var item = new Item('item');
 		item.quality = 10;
-		updater.addItem(item);
-		updater.update(updater.items);
+		updateItem(item);
 		expect(item.quality).toBe(9);
+	});
+
+	it("decreases item sell-in on update", function() {
+		var item = new Item('item');
+		item.sell_in = 10;
+		updateItem(item);
+		expect(item.sell_in).toBe(9);
+	});
+
+	it("does not allow item quality to be less than zero", function() {
+		var item = new Item('item');
+		item.quality = 0;
+		updateItem(item);
+		expect(item.quality).toBe(0);
+	});
+
+	it("doubles quality decrease when sell-in is zero", function() {
+		var item = new Item('item');
+		item.sell_in = 0;
+		item.quality = 10;
+		updateItem(item);
+		expect(item.quality).toBe(8);
 	});
 });
